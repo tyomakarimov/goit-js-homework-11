dc27bef21ba7ce31446b2808dbbaf5a0fbc3e5ab
tyomakarimov <karartvas170202@gmail.com>
2021-12-26 18:01:08 +0200

    Add: pagination and notifications

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 'use strict';
+
 import 'regenerator-runtime/runtime';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 import { getImages, getBlobData } from './api/api-helper.js';
 
 const form = document.getElementById('search-form');
+const submitButton = document.getElementById('submit-button');
 const input = document.querySelector('input');
 const galleryList = document.getElementById('image-list');
+const loadMoreButton = document.getElementById('load-more');
+loadMoreButton.style.display = 'none';
 
-form.addEventListener('submit', async event => {
+const loadMoreButtonDetails = {
+  page: 1,
+};
+
+submitButton.addEventListener('click', () => (loadMoreButtonDetails.page = 1));
+
+const submitHandler = async event => {
   event.preventDefault();
-  galleryList.innerHTML = '';
+  loadMoreButton.style.display = 'none';
   const searchPhrase = input.value;
-  const images = await getImages(searchPhrase, 1);
+  const images = await getImages(searchPhrase, loadMoreButtonDetails.page);
+  if (loadMoreButtonDetails.page === 1) {
+    galleryList.innerHTML = '';
+    if (images.hits.length) Notify.success(`Hooray! We found ${images.totalHits} images.`);
+    else Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+  } else {
+    if (!images.hits.length) {
+      Notify.failure(`We're sorry, but you've reached the end of search results.`);
+    }
+  }
+  loadMoreButtonDetails.page++;
   for (const image of images.hits) {
     const blob = await getBlobData(image.webformatURL);
     const url = URL.createObjectURL(blob);
@@ -41,4 +62,9 @@ form.addEventListener('submit', async event => {
       </li>
     `;
   }
-});
+  if (images.hits.length) loadMoreButton.style.display = 'block';
+};
+
+loadMoreButton.addEventListener('click', submitHandler);
+
+form.addEventListener('submit', submitHandler);
