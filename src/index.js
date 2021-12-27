e3eebffcf4fd20578120944d198889aa93af7206
tyomakarimov <karartvas170202@gmail.com>
2021-12-27 22:35:46 +0200

    Add: helper function for getting image item template

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'regenerator-runtime/runtime';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 import { getImages, getBlobData } from './api/api-helper.js';
+import getImageItem from './helpers/image-item-template.js';
 
 const form = document.getElementById('search-form');
 const submitButton = document.getElementById('submit-button');
@@ -36,31 +37,7 @@ const submitHandler = async event => {
   for (const image of images.hits) {
     const blob = await getBlobData(image.webformatURL);
     const url = URL.createObjectURL(blob);
-    galleryList.innerHTML += `
-      <li class="image-item">
-        <div class="photo-card">
-          <img src="${url}" alt="${image.tags}" loading="lazy" width="369.8" height="254" />
-          <div class="info">
-            <div class="info-item">
-              <b class="label">Likes</b>
-              <p class="value">${image.likes}</p>
-            </div>
-            <div class="info-item">
-              <b class="label">Views</b>
-              <p class="value">${image.views}</p>
-            </div>
-            <div class="info-item">
-              <b class="label">Comments</b>
-              <p class="value">${image.comments}</p>
-            </div>
-            <div class="info-item">
-              <b class="label">Downloads</b>
-              <p class="value">${image.downloads}</p>
-            </div>
-          </div>
-        </div>
-      </li>
-    `;
+    galleryList.innerHTML += getImageItem(url, image);
   }
   if (images.hits.length) loadMoreButton.style.display = 'block';
 };
